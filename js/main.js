0370def7557711e9e085c03909523a73780ee7c4
Óscar García-bermejo <114060035+Osgabede@users.noreply.github.com>
2024-11-22 12:24:09 +0100

    v.1.0 Finished the basics
    Added wall death, fruit spawns and snake length increase.
    To Implement: Difficulty settings and UI

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 const board = document.querySelector("#board");
 const snake = [];
-let snakeLength = 15;
+const boardSize = 37; // might initialize as let instead of const to make the progressive difficulty reduce the board size
+let snakeLength = 10;
+const startPosition = Math.round((boardSize*boardSize)/2-snakeLength);
 let snakeDirection;
+let snakeSpeed = 70; // the miliseconds between each cell jump (lower = faster)
+let gameOver;
 
-for (let i = 0; i < 50 * 50; i++) {
+
+// setup the board grid
+board.style.gridTemplate = `repeat(${boardSize}, 1fr) / repeat(${boardSize}, 1fr)`
+// setup the board divs
+for (let i = 0; i < boardSize * boardSize; i++) {
   const cellDiv = document.createElement("div");
   cellDiv.classList.add('cell');
   cellDiv.id = `cell-${i}`;
-  cellDiv.style.border = "solid 0.5px grey";
   board.appendChild(cellDiv);
 }
 
-// start the game
-const startPosition = 1257;
+// draw the snake in starting position
 for (let i = 0; i < snakeLength; i++) {
   const snakeCell = document.querySelector(`#cell-${startPosition+i}`);
   snake[i] = snakeCell;
   snake[i].classList.add('snake');
 }
-// ---------------
+
+let snakeHead = snake[snakeLength-1];
 
 document.addEventListener('keydown', function(event) {  // on any key press
   const pressedKey = event.key.toLowerCase();  // save the key pressed on pressedKey
@@ -27,74 +34,98 @@ document.addEventListener('keydown', function(event) {  // on any key press
   }
 })
 
-let intervalID = setInterval(function updateSnake() {
+respawnFruit(); // first fruit
+
+const updateSnakeInterval = setInterval(() => {
+
+  let divToAdd;
+  snakeHead.classList.remove('snake-head'); // remove class from snake head
+
   if (snakeDirection === 'd') {
 
-    const divToAdd = document.querySelector(`#cell-${parseInt(snake[snakeLength-1].id.slice(5))+1}`); // select the right div
-    
-    if (!divToAdd.classList.contains('snake')) { // can move that way
+    divToAdd = document.querySelector(`#cell-${parseInt(snakeHead.id.slice(5))+1}`); // select the right div
 
+    if (divToAdd.classList.contains('snake') || !Boolean((parseInt(snakeHead.id.slice(5))+1) % boardSize)) { // snake bites itself or hits right wall
+      gameOver = true; // set game over
+
+    } else {  // snake moves without problem
       divToAdd.classList.add('snake'); // add the snake class to it
       snake.push(divToAdd); // add it to the snake array
-
-      const divToRemove = snake.shift(); // remove the div on the snake's tail
-      divToRemove.classList.remove('snake'); // remove the snake class from it
-
-    } else {  // snake bites itself
-      clearInterval(intervalID);
-      alert('game over');
     }
 
   } else if (snakeDirection === 'a') {
 
-    const divToAdd = document.querySelector(`#cell-${parseInt(snake[snakeLength-1].id.slice(5))-1}`); // select the right div
+    divToAdd = document.querySelector(`#cell-${parseInt(snakeHead.id.slice(5))-1}`); // select the right div
     
-    if (!divToAdd.classList.contains('snake')) { // can move that way
+    if (divToAdd.classList.contains('snake') || !Boolean(parseInt(snakeHead.id.slice(5)) % boardSize)) { // snake bites itself or hits left wall
+      gameOver = true; // set game over
 
+    } else {  // snake moves without problem
       divToAdd.classList.add('snake'); // add the snake class to it
       snake.push(divToAdd); // add it to the snake array
-
-      const divToRemove = snake.shift(); // remove the div on the snake's tail
-      divToRemove.classList.remove('snake'); // remove the snake class from it
-
-    } else {  // snake bites itself
-      clearInterval(intervalID);
-      alert('game over');
     }
 
   } else if (snakeDirection === 'w') {
 
-    const divToAdd = document.querySelector(`#cell-${parseInt(snake[snakeLength-1].id.slice(5))-50}`); // select the right div
-    
-    if (!divToAdd.classList.contains('snake')) { // can move that way
+    divToAdd = document.querySelector(`#cell-${parseInt(snakeHead.id.slice(5))-boardSize}`); // select the right div
 
+    if (!divToAdd || divToAdd.classList.contains('snake')) { // snake bites itself or hits top wall
+      gameOver = true; // set game over
+
+    } else {  // snake moves without problem
       divToAdd.classList.add('snake'); // add the snake class to it
       snake.push(divToAdd); // add it to the snake array
-
-      const divToRemove = snake.shift(); // remove the div on the snake's tail
-      divToRemove.classList.remove('snake'); // remove the snake class from it
-
-    } else {  // snake bites itself
-      clearInterval(intervalID);
-      alert('game over');
     }
 
   } else if (snakeDirection === 's') {
+    
+    divToAdd = document.querySelector(`#cell-${parseInt(snakeHead.id.slice(5))+boardSize}`); // select the right div
 
-    const divToAdd = document.querySelector(`#cell-${parseInt(snake[snakeLength-1].id.slice(5))+50}`); // select the right div
+    if (!divToAdd || divToAdd.classList.contains('snake')) { // snake bites itself or hits bottom wall
+      gameOver = true; // set game over
 
-    if (!divToAdd.classList.contains('snake')) { // can move that way
-
+    } else {  // snake moves without problem
       divToAdd.classList.add('snake'); // add the snake class to it
       snake.push(divToAdd); // add it to the snake array
+    }
+
+  }
+
+  // condition for game over
+  if (gameOver) {
+    clearInterval(updateSnakeInterval);
+    alert('Game Over');
+  } else if (divToAdd) { // condition for snake growth (if snake moved)
+    if (!divToAdd.classList.contains('fruit')) { // snake moved to a cell without a fruit
 
       const divToRemove = snake.shift(); // remove the div on the snake's tail
       divToRemove.classList.remove('snake'); // remove the snake class from it
 
-    } else {  // snake bites itself
-      clearInterval(intervalID);
-      alert('game over');
+    } else { // snake moved to a cell with a fruit
+      respawnFruit(); // respawn a fruit
+      snakeLength++; // increase snake length
     }
-
   }
-}, 300);
+  
+  // update snake head
+  snakeHead = snake[snakeLength-1];
+  snakeHead.classList.add('snake-head');
+
+}, snakeSpeed);
+
+function respawnFruit () {
+
+  if (document.querySelector('.fruit')) {
+    document.querySelector('.fruit').classList.remove('fruit');
+  }
+
+  const randNum = Math.floor(Math.random() * ((boardSize*boardSize)+1)); // generate a random number between 0 and 2500
+  let randCellDiv;
+  do {
+
+    randCellDiv = document.querySelector(`#cell-${randNum}`); // select a random cell 
+
+  } while (randCellDiv.classList.contains('snake')); // if it selected a cell thats part of snake, reselect
+
+  randCellDiv.classList.add('fruit');
+}
